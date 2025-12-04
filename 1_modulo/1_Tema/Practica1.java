6f74e2580f37dc1dbd021af09e278f33591fd268
Angelo Dylan <angelo.quispe.l@tecsup.edu.pe>
2025-12-04 08:48:43 -0500

    Feat: Realizar practica del tema 1

---
+++ 1_modulo/1_Tema/Practica1.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Practica1 {
+  public static void main(String[] args) {
+    JFrame ventana = new JFrame();
+
+    ventana.setTitle("Venta de Desarrollo");
+    ventana.setSize(300,600);
+    //ventana.setLocationRelativeTo(null);
+    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    ventana.setVisible(true);
+  }
+}
